696146b5621dc3404d56be3b70e3e8547c8ba5e0
sohan-shingade <sohanshingade@gmail.com>
2020-07-15 12:04:10 -0700

    cool changes

--- App.js
+++ App.js
@@ -1,75 +1,80 @@
-import React from "react";
-import { Text, TouchableOpacity, FlatList, SafeAreaView } from "react-native";
-import * as Linking from "expo-linking";
+import React from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+  Platform,
+} from 'react-native';
+import * as Linking from 'expo-linking';
 
 const links = [
   {
-    title: "Plus",
-    url: "https://www.Walmart.com/plus",
-    color: "#d88d8a",
+    title: 'Plus',
+    url: 'https://www.Walmart.com/plus',
+    color: '#d88d8a',
   },
   {
-    title: "Plus Faq",
-    url: "https://www.Walmart.com/plus/faq",
-    color: "yellow",
+    title: 'Plus Faq',
+    url: 'https://www.Walmart.com/plus/faq',
+    color: 'yellow',
   },
   {
-    title: "Trial Plans",
-    url: "https://www.Walmart.com/plus/faq/trial-membership-plans",
-    color: "gray",
+    title: 'Trial Plans',
+    url: 'https://www.Walmart.com/plus/faq/trial-membership-plans',
+    color: 'gray',
   },
   {
-    title: "Plus Plans",
-    url: "https://www.Walmart.com/plus/faq/walmart-plus-membership-plans",
-    color: "orange",
+    title: 'Plus Plans',
+    url: 'https://www.Walmart.com/plus/faq/walmart-plus-membership-plans',
+    color: 'orange',
   },
   {
-    title: "Payment and Billing",
-    url: "https://www.Walmart.com/plus/faq/payment-and-billing",
-    color: "cyan",
+    title: 'Payment and Billing',
+    url: 'https://www.Walmart.com/plus/faq/payment-and-billing',
+    color: 'cyan',
   },
   {
-    title: "Prior Delivery Unlimited Customers",
-    url: "https://www.Walmart.com/plus/faq/prior-delivery-unlimited-customers",
-    color: "red",
+    title: 'Prior Delivery Unlimited Customers',
+    url: 'https://www.Walmart.com/plus/faq/prior-delivery-unlimited-customers',
+    color: 'red',
   },
   {
-    title: "Prior Delivery Unlimited Customers",
-    url: "https://www.Walmart.com/plus/faq/prior-delivery-unlimited-customers",
-    color: "#b19cd9",
+    title: 'Prior Delivery Unlimited Customers',
+    url: 'https://www.Walmart.com/plus/faq/prior-delivery-unlimited-customers',
+    color: '#b19cd9',
   },
   {
-    title: "Free Unlimited Delivery",
-    url: "https://www.Walmart.com/plus/faq/free-unlimited-delivery",
-    color: "#b5651d",
+    title: 'Free Unlimited Delivery',
+    url: 'https://www.Walmart.com/plus/faq/free-unlimited-delivery',
+    color: '#b5651d',
   },
   {
-    title: "Prefered Time Slots",
-    url: "https://www.Walmart.com/plus/faq/preferred-time-slots",
-    color: "#2f96a3",
+    title: 'Prefered Time Slots',
+    url: 'https://www.Walmart.com/plus/faq/preferred-time-slots',
+    color: '#2f96a3',
   },
   {
-    title: "Open Time Slots",
-    url: "https://www.Walmart.com/plus/faq/open-time-slot-notifications",
-    color: "#81b887",
+    title: 'Open Time Slots',
+    url: 'https://www.Walmart.com/plus/faq/open-time-slot-notifications',
+    color: '#81b887',
   },
   {
-    title: "Scan and Go",
-    url: "https://www.Walmart.com/plus/faq/mobile-scan-and-go",
-    color: "#baefc6",
+    title: 'Scan and Go',
+    url: 'https://www.Walmart.com/plus/faq/mobile-scan-and-go',
+    color: '#baefc6',
   },
   {
-    title: "Fuel Discounts",
-    url: "https://www.Walmart.com/plus/faq/fuel-discounts",
-    color: "#20ee9c",
+    title: 'Fuel Discounts',
+    url: 'https://www.Walmart.com/plus/faq/fuel-discounts',
+    color: '#20ee9c',
   },
   {
-    title: "Early Access Deals",
-    url: "https://www.Walmart.com/plus/faq/early-access-to-deals",
-    color: "#61d308",
-  }
-]
-
+    title: 'Early Access Deals',
+    url: 'https://www.Walmart.com/plus/faq/early-access-to-deals',
+    color: '#61d308',
+  },
+];
 
 export default class App extends React.Component {
   handlePress = (url) => {
@@ -92,10 +97,14 @@ export default class App extends React.Component {
         width: 300,
         height: 100,
         backgroundColor: item.color,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
-    >
+      onPress={() => {
+        if (Platform.OS !== 'web') {
+          this.handlePress(item.url);
+        }
+      }}>
       <Text accessibilityRole="link" href={item.url} target="_blank">
         {item.title}
       </Text>
@@ -107,10 +116,9 @@ export default class App extends React.Component {
       <SafeAreaView
         style={{
           flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
         <FlatList
           data={links}
           renderItem={(item) => this.ButtonLink(item.item)}
--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
   };
-=======
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
->>>>>>> 2dce8e210cbf420bae9a957f9a025a01cb1a034d
 };
